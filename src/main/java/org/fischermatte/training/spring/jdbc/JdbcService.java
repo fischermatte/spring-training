f4a5d3dad70fa2a0200f08d6f505b72154e03ce7
fischermatte <fischermatte@posteo.net>
2016-10-09 14:51:23 +0200

    played around with transactiontemplate and transactionmanager

--- src/main/java/org/fischermatte/training/spring/jdbc/JdbcService.java
+++ src/main/java/org/fischermatte/training/spring/jdbc/JdbcService.java
@@ -3,26 +3,48 @@ package org.fischermatte.training.spring.jdbc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.util.List;
 
 @Service
 public class JdbcService {
     private final JdbcTemplate jdbcTemplate;
+    private final PlatformTransactionManager transactionManager;
+    private final TransactionTemplate transactionTemplate;
 
     @Autowired
-    public JdbcService(JdbcTemplate jdbcTemplate) {
+    public JdbcService(JdbcTemplate jdbcTemplate, PlatformTransactionManager transactionManager, TransactionTemplate transactionTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+        this.transactionManager = transactionManager;
+        this.transactionTemplate = transactionTemplate;
     }
 
     public void doSomeJdbc() {
         jdbcTemplate.execute("create table employee (id int, name varchar)");
-        jdbcTemplate.execute("insert into employee (id, name) values (1, 'A')");
-        jdbcTemplate.execute("insert into employee (id, name) values (2, 'B')");
-        jdbcTemplate.execute("insert into employee (id, name) values (3, 'C')");
-        jdbcTemplate.execute("insert into employee (id, name) values (4, 'D')");
-        jdbcTemplate.execute("insert into employee (id, name) values (5, 'E')");
-        jdbcTemplate.execute("insert into employee (id, name) values (6, 'F')");
+//
+        transactionTemplate.execute(status -> {
+            jdbcTemplate.execute("insert into employee (id, name) values (1, 'A')");
+            jdbcTemplate.execute("insert into employee (id, name) values (2, 'B')");
+            jdbcTemplate.execute("insert into employee (id, name) values (3, 'C')");
+            jdbcTemplate.execute("insert into employee (id, name) values (4, 'D')");
+            jdbcTemplate.execute("insert into employee (id, name) values (5, 'E')");
+            jdbcTemplate.execute("insert into employee (id, name) values (6, 'F')");
+            return null;
+        });
+
+
+
+        TransactionDefinition transactionDefintion = new DefaultTransactionDefinition(transactionTemplate);
+        TransactionStatus transactionStatus = transactionManager.getTransaction(transactionDefintion);
+        jdbcTemplate.execute("insert into employee (id, name) values (7, 'H')");
+        jdbcTemplate.execute("insert into employee (id, name) values (8, 'I')");
+        transactionManager.rollback(transactionStatus);
+
 
         // row mapper
         List<String> names = jdbcTemplate.query("SELECT * FROM employee", (resultSet, i) -> resultSet.getString("name"));
